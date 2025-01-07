56e303c49b2f79a0fd43db1d41ba9f3198fe16c8
Eric Almendral <eralmendral@gmail.com>
2025-01-07 22:35:38 +0800

    [be]: Go language variables

---
+++ Backend/Go Language/Exercises/GO_TOUR/basic-types.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
+
+
+/*
+Numeric types¶
+An integer, floating-point, or complex type represents the set of integer, floating-point, or complex values, respectively. They are collectively called numeric types. The predeclared architecture-independent numeric types are:
+
+uint8       the set of all unsigned  8-bit integers (0 to 255)
+uint16      the set of all unsigned 16-bit integers (0 to 65535)
+uint32      the set of all unsigned 32-bit integers (0 to 4294967295)
+uint64      the set of all unsigned 64-bit integers (0 to 18446744073709551615)
+
+int8        the set of all signed  8-bit integers (-128 to 127)
+int16       the set of all signed 16-bit integers (-32768 to 32767)
+int32       the set of all signed 32-bit integers (-2147483648 to 2147483647)
+int64       the set of all signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+float32     the set of all IEEE 754 32-bit floating-point numbers
+float64     the set of all IEEE 754 64-bit floating-point numbers
+
+complex64   the set of all complex numbers with float32 real and imaginary parts
+complex128  the set of all complex numbers with float64 real and imaginary parts
+
+byte        alias for uint8
+rune        alias for int32
+
+*/
+
+
+/*
+
+Type conversions
+The expression T(v) converts the value v to the type T.
+
+Some numeric conversions:
+
+var i int = 42
+var f float64 = float64(i)
+var u uint = uint(f)
+Or, put more simply:
+
+i := 42
+f := float64(i)
+u := uint(f)
+Unlike in C, in Go assignment between items of different type requires an explicit conversion. Try removing the float64 or uint conversions in the example and see what happens.
+
+*/
+
+/*
+
+Type inference
+When declaring a variable without specifying an explicit type (either by using the := syntax or var = expression syntax), the variable's type is inferred from the value on the right hand side.
+
+When the right hand side of the declaration is typed, the new variable is of that same type:
+
+var i int
+j := i // j is an int
+But when the right hand side contains an untyped numeric constant, the new variable may be an int, float64, or complex128 depending on the precision of the constant:
+
+i := 42           // int
+f := 3.142        // float64
+g := 0.867 + 0.5i // complex128
+Try changing the initial value of v in the example code and observe how its type is affected.
+
+
+*/
---
+++ Backend/Go Language/Exercises/GO_TOUR/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var i,j int = 1,10
+
+	fmt.Println(i,j)
+}
