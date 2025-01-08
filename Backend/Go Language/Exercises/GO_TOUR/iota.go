37ea0d9eef9e70e6827f0baf510e2589469d67bb
Eric Almendral <eralmendral@gmail.com>
2025-01-08 23:55:50 +0800

    [be]: Go iota exercise

---
+++ Backend/Go Language/Exercises/GO_TOUR/iota.go
@@ -0,0 +1,6 @@
+/*
+	Create a program that uses iota to calculate the size of each measurement of bytes.
+	KB, MB, GB, TB, PB, EB, ZB, YB
+
+	- show the sizes of each in Decimal, Binary and Hexadecimal
+*/
