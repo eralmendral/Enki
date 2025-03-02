050370747beaff7574a86024adf62dfb93a8238d
Eric Almendral <eralmndrx@gmail.com>
2025-03-03 07:23:22 +0800

    [be] go generics

---
+++ Backend/Go Language/Exercises/Go_Generics/main.go
@@ -0,0 +1,20 @@
+package main
+
+func addInt(a, b int) int {
+	return a + b
+}
+
+func addFloat(a, b float64) float64 {
+	return a + b
+}
+
+func addGeneric[T int | float64](a, b T) T {
+	return a + b
+}
+
+func main() {
+	println(addInt(1, 2))
+	println(addFloat(1.1, 2.2))
+	println(addGeneric(1, 2))
+	println(addGeneric(1.1, 2.2))
+}
