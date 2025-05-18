07aa58989036253054081ccf33be9378a75e4ca2
Eric Almendral <27731112+eralmendral@users.noreply.github.com>
2025-05-18 12:26:38 +0800

    Update linkedlist-queue.py

--- Computer Science/Data Structures and Algorithms/LinkedList/linkedlist-queue.py
+++ Computer Science/Data Structures and Algorithms/LinkedList/linkedlist-queue.py
@@ -1,1 +1,7 @@
 
+Todo:
+add_to_head should set the tail reference to the given node if the list is empty.
+add_to_tail should:
+Set the head and tail to the given node if the list is empty
+Instead of iterating to find the last node, use the tail reference to append the new node
+The constructor should set self.tail to None.
